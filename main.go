c886b7786d3b84e613b90577e7f2f6581ef38442
netaka <developer@netaka.net>
2021-06-07 12:33:19 +0000

    Retab

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+    "flag"
+    "fmt"
     "encoding/json"
     _ "embed"
-	"github.com/netaka/pomato/line"
-	"github.com/netaka/pomato/slack"
+    "github.com/netaka/pomato/line"
+    "github.com/netaka/pomato/slack"
 )
 
 var (
-	message     = flag.String("m", "undefined", "message string")
-	enableSlack = flag.Bool("s", false, "enable posting to Slack")
-	enableLine  = flag.Bool("l", false, "enable posting to LINE")
+    message     = flag.String("m", "undefined", "message string")
+    enableSlack = flag.Bool("s", false, "enable posting to Slack")
+    enableLine  = flag.Bool("l", false, "enable posting to LINE")
 )
 
 //go:embed token.json
@@ -29,15 +29,15 @@ type token struct {
 func main() {
     var t token
     json.Unmarshal([]byte(tokenData), &t)
-	slack.Init(t.slack_token, t.slack_channel)
-	line.Init(t.line_channel_access_token, t.line_channel_secret, t.line_user_id)
+    slack.Init(t.slack_token, t.slack_channel)
+    line.Init(t.line_channel_access_token, t.line_channel_secret, t.line_user_id)
 
-	flag.Parse()
-	fmt.Println(*message)
-	if *enableSlack {
-		slack.Post(*message)
-	}
-	if *enableLine {
-		line.Post(*message)
-	}
+    flag.Parse()
+    fmt.Println(*message)
+    if *enableSlack {
+        slack.Post(*message)
+    }
+    if *enableLine {
+        line.Post(*message)
+    }
 }
