d8af046bfd8f5ac724b57eda0133f88907a781c9
netaka <developer@netaka.net>
2021-06-07 13:43:16 +0000

    Fix to unmarshal json file correctly

--- main.go
+++ main.go
@@ -19,18 +19,22 @@ var (
 var tokenData []byte
 
 type token struct {
-    slack_token string `json:"slack_token"`
-    slack_channel string `json:"slack_channel"`
-    line_channel_access_token string `json:"line_channel_access_token"`
-    line_channel_secret string `json:"line_channel_secret"`
-    line_user_id string `json:"line_user_id"`
+    SlackToken string `json:"slack_token"`
+    SlackChannel string `json:"slack_channel"`
+    LineChannelAccessToken string `json:"line_channel_access_token"`
+    LineChannelSecret string `json:"line_channel_secret"`
+    LineUserId string `json:"line_user_id"`
 }
 
 func main() {
     var t token
-    json.Unmarshal([]byte(tokenData), &t)
-    slack.Init(t.slack_token, t.slack_channel)
-    line.Init(t.line_channel_access_token, t.line_channel_secret, t.line_user_id)
+    err := json.Unmarshal(tokenData, &t)
+    if err != nil {
+        fmt.Printf("error: %#v\n", err)
+    }
+
+    slack.Init(t.SlackToken, t.SlackChannel)
+    line.Init(t.LineChannelAccessToken, t.LineChannelSecret, t.LineUserId)
 
     flag.Parse()
     fmt.Println(*message)
